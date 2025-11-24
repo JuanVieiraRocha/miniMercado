1610ef85edf5424cd1a134e10525df2f8af65c0f
Juanzito <danieljruan@gmail.com>
2025-11-23 23:43:46 -0300

    inicio do JavaScript

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
+class Produto {
+    constructor(nome, valor, botao){
+        this.nome = nome
+        this.valor = valor
+        this.botao = botao
+    }
+}
+
 const cards = document.getElementsByClassName('cartao')
 
 let produtosNoCarrinho = []
+let produtos
 
-for (let card of cards) {
-    card.addEventListener('mouseenter', () => {
-        card.style.transform = "scale(1.5)";
-        card.style.zIndex = "10";
-    })
+if(cards){
+    for (let card of cards) {
+        card.addEventListener('mouseenter', () => {
+            card.style.transform = "scale(1.5)";
+            card.style.zIndex = "10";
+        })
 
-      card.addEventListener('mouseleave', () => {
-    card.style.transform = "scale(1)";
-    card.style.zIndex = "1";
-        
-    })
-    let botoes = card.querySelector("button")
+        card.addEventListener('mouseleave', () => {
+        card.style.transform = "scale(1)";
+        card.style.zIndex = "1";
+            
+        })
+        let botoes = card.querySelector("button")
+        let valor = card.querySelector("b")
+        let nome = card.querySelector("p")
 
-    botoes.addEventListener('click', () => {
-        produtosNoCarrinho.push(card)
-        console.log(produtosNoCarrinho)
-        localStorage.setItem("produto", JSON.stringify(produtosNoCarrinho))
-    })
+        botoes.addEventListener('click', () => {
+            
+            produtosNoCarrinho.push(new Produto(nome.textContent, valor.textContent, botoes))
+            console.log(produtosNoCarrinho)
+            localStorage.setItem("produto", JSON.stringify(produtosNoCarrinho))
+        })
+
+
+    }
 }
