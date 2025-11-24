9c642ebf19504e42fc348baaf9c6118b7a20fa79
Juanzito <danieljruan@gmail.com>
2025-11-23 21:15:26 -0300

    safesave

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const cards = document.querySelectorAll('cartao')
+const cards = document.getElementsByClassName('cartao')
 
-cards.addEventListener('hover', () =>{
-    
-})
+let produtosNoCarrinho = []
+
+for (let card of cards) {
+    card.addEventListener('mouseenter', () => {
+        card.style.transform = "scale(1.5)";
+        card.style.zIndex = "10";
+    })
+
+      card.addEventListener('mouseleave', () => {
+    card.style.transform = "scale(1)";
+    card.style.zIndex = "1";
+        
+    })
+    let botoes = card.querySelector("button")
+
+    botoes.addEventListener('click', () => {
+        produtosNoCarrinho.push(card)
+        console.log(produtosNoCarrinho)
+        localStorage.setItem("produto", JSON.stringify(produtosNoCarrinho))
+    })
+}
