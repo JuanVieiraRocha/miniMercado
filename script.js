4df9aee75878bc40d32654a456cc13d609367fd8
Juan <130806027+JuanVieiraRocha@users.noreply.github.com>
2025-11-24 15:39:21 -0300

    adicionar novos produtos

--- script.js
+++ script.js
@@ -6,16 +6,30 @@ class Produto {
     }
 }
 
+//IDS' da tela de inicio
 const cards = document.getElementsByClassName('cartao')
 const carrinho = document.getElementById('produtos')
 const abrir = document.getElementById('Adicionar')
+
+//ID's da modal
 const fechar = document.getElementById('fechar')
 const modalDeCriarProduto = document.getElementById('criarProduto')
 const enviar = document.getElementById('enviar')
+const nome = document.getElementById('nome')
+const valor = document.getElementById('valor')
+const comida = document.getElementById('alimento')
+const bebida = document.getElementById('liquido')
+const limpeza = document.getElementById('limpo')
+
+//ID's das sessões
+const alimenticios = document.getElementById('comida')
+const liquidos = document.getElementById('bebida')
+const limpo = document.getElementById('limpeza')
 
 let produtosNoCarrinho = []
 localStorage.setItem("produto", JSON.stringify(produtosNoCarrinho))
 let produtos = []
+localStorage.setItem('loja', JSON.stringify(produtos))
 
 if(cards){
     for (let card of cards) {
@@ -50,16 +64,32 @@ function adicionarAoCarrinho(produto){
     localStorage.setItem('produto', JSON.stringify(produtosNoCarrinho))
 } // adiciona itens ao array de carrinho
 
-//função de adicionar produtos à loja
-const nome = document.getElementById('nome')
-const valor = document.getElementById('valor')
-const comida = document.getElementById('alimento')
-const bebida = document.getElementById('liquido')
-const limpeza = document.getElementById('limpo')
 
+// Funções de adicionar produtos à loja
+function adicionarProdutoALoja(nome){
+    let produtos = JSON.parse(localStorage.getItem('loja')) || []
+    console.log = produtos
+    let produtoEscolhido
+    for(produto of produtos){
+        if(produto.nome == nome){
+            produtoEscolhido = produto
+            break
+        }
+    }
+    let card = document.createElement('div')
+    let nam = document.createElement('p')
+    let preco = document.createElement('b')
+    let adiciona = document. createElement('button')
 
+    card.className = 'cartao'
+    nam.textContent = produto.nome
+    preco.textContent = produto.valor
+    adiciona.textContent = 'Adicionar ao carrinho'
 
-function adicionarProdutoALoja(){
+    card.appendChild(nam)
+    card.appendChild(preco)
+    card.appendChild(adiciona)
+    alimenticios.appendChild(card)
 
 }
 
@@ -77,5 +107,26 @@ fechar.addEventListener('click', () =>{
     limpeza.value = ''
 })
 enviar.addEventListener('click', () =>{
+    let produtos = JSON.parse(localStorage.getItem('loja')) || []
+    let novoProduto;
+    if(comida.checked){
+        novoProduto = new Produto(nome.value, valor.value, comida.value)
+    }
+    if(bebida.checked){
+        novoProduto = new Produto(nome.value, valor.value, bebida.value)
+    }
+    if(limpeza.checked){
+        novoProduto = new Produto(nome.value, valor.value, bebida.value)
+    }
 
+    produtos.push(novoProduto)
+    localStorage.setItem('loja', JSON.stringify(produtos))
+    modalDeCriarProduto.close()
+    modalDeCriarProduto.style.display = 'none'
+    adicionarProdutoALoja(novoProduto.nome)
+    nome.value = ''
+    valor.value = ''
+    bebida.checked = false
+    comida.checked = false
+    limpeza.checked = false
 })
